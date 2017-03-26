eb01d4294b2fed1aacc04d82f6df74ad2edfaa7d
Ozbroome <eric.rodriguez.pp@gmail.com>
2017-03-26 18:06:30 +0200

    amélioration code service recaptcha

--- src/BlogBundle/Services/Recaptcha.php
+++ src/BlogBundle/Services/Recaptcha.php
@@ -3,25 +3,29 @@ namespace BlogBundle\Services;
 
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class Recaptcha
 {
 
     private $secret;
+    protected $requestStack;
 
-    public function __construct($secret)
+    public function __construct($secret, RequestStack $requestStack)
     {
         $this->secret = $secret;
+        $this->requestStack = $requestStack;
     }
 
     public function verifCaptcha()
     {
+        $request = $this->requestStack->getCurrentRequest();
         // Ma clé privée
         $secret = $this->secret;
         // Paramètre renvoyé par le recaptcha
-        $response = $_POST['g-recaptcha-response'];
+        $response = $request->request->get('g-recaptcha-response');
         // On récupère l'IP de l'utilisateur
-        $remoteip = $_SERVER['REMOTE_ADDR'];
+        $remoteip = $request->server->get('REMOTE_ADDR');
 
         $api_url = "https://www.google.com/recaptcha/api/siteverify?secret="
             . $secret
